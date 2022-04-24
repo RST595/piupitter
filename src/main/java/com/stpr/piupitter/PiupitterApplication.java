4d6290b0f01db22d31ee0dff1aa9531542ca7143
Maxim <rst>
2022-04-24 13:25:09 +0300

    Initial commit

---
+++ src/main/java/com/stpr/piupitter/PiupitterApplication.java
@@ -0,0 +1,13 @@
+package com.stpr.piupitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiupitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiupitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stpr/piupitter/PiupitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpr.piupitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiupitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
